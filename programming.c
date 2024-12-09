669235a14dfbb55d6df70f54e46cf6486a3664cf
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-12-09 21:36:52 +0600

    nested loop

--- programming.c
+++ programming.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d", &n);
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
 
-    if(n >=3 ){
-        int baki_day = n - 3;
-        if(baki_day >= 2){
-            printf("Saintmartin + cox's bazar eksate jabo");
-        }
-        else{
-        printf("Tour dibo saintmartin");
-        }
-    }
-    else if(n >= 2){
-        printf("Tour dibo Cox's Bazar");
-    }
-    else if(n >= 1){
-        printf("Basai gumabo");
-    }
-    else{
-        printf("kisoi hbe na");
-    }
-}
+//     if(n >=3 ){
+//         int baki_day = n - 3;
+//         if(baki_day >= 2){
+//             printf("Saintmartin + cox's bazar eksate jabo");
+//         }
+//         else{
+//         printf("Tour dibo saintmartin");
+//         }
+//     }
+//     else if(n >= 2){
+//         printf("Tour dibo Cox's Bazar");
+//     }
+//     else if(n >= 1){
+//         printf("Basai gumabo");
+//     }
+//     else{
+//         printf("kisoi hbe na");
+//     }
+// }
