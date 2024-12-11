dffc4112dc27cc2f5740516338b5df282c001fda
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-12-11 19:55:47 +0600

    m

--- programming.c
+++ programming.c
@@ -24,6 +24,32 @@
 //     }
 // }
 
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     if(n >=3 ){
+//         int baki_day = n - 3;
+//         if(baki_day >= 2){
+//             printf("Saintmartin + cox's bazar eksate jabo");
+//         }
+//         else{
+//         printf("Tour dibo saintmartin");
+//         }
+//     }
+//     else if(n >= 2){
+//         printf("Tour dibo Cox's Bazar");
+//     }
+//     else if(n >= 1){
+//         printf("Basai gumabo");
+//     }
+//     else{
+//         printf("kisoi hbe na");
+//     }
+// }
+
 #include<stdio.h>
 int main()
 {
